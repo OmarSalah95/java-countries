0b37d0236f6258a5ce3e13ec255a4f12ac3fa671
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-08-12 13:36:26 -0700

    Initial commit intialized all necesary objects and controllers, as well as built base get endpoints.

---
+++ countries/src/main/java/com/countries/countries/CountryController.java
@@ -0,0 +1,27 @@
+package com.countries.countries;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
+
+@RestController
+
+public class CountryController
+{
+	@GetMapping(value = "allcountries", produces = {"application/json"})
+	public ResponseEntity<?> getAllCountriesSorted()
+	{
+		CountriesApplication.ourCountryList.countryList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
+		return new ResponseEntity<>(CountriesApplication.ourCountryList.countryList, HttpStatus.OK);
+	}
+
+	@GetMapping(value = "", produces = {"application/json"})
+	public ResponseEntity<?> getAllCountriesSorted()
+	{
+		CountriesApplication.ourCountryList.countryList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
+		return new ResponseEntity<>(CountriesApplication.ourCountryList.countryList, HttpStatus.OK);
+	}
+}
