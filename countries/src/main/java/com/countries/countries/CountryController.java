45d380fad6d3121fc56033539d822fa76bcebb1b
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-08-12 13:54:07 -0700

    Added a couple more endpoints to the Names controller

--- countries/src/main/java/com/countries/countries/CountryController.java
+++ countries/src/main/java/com/countries/countries/CountryController.java
@@ -3,25 +3,43 @@ package com.countries.countries;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
+import java.util.ArrayList;
 
 @RestController
-
+@RequestMapping("/names")
 public class CountryController
 {
-	@GetMapping(value = "allcountries", produces = {"application/json"})
+	@GetMapping(value = "/all", produces = {"application/json"})
 	public ResponseEntity<?> getAllCountriesSorted()
 	{
 		CountriesApplication.ourCountryList.countryList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
 		return new ResponseEntity<>(CountriesApplication.ourCountryList.countryList, HttpStatus.OK);
 	}
 
-	@GetMapping(value = "", produces = {"application/json"})
-	public ResponseEntity<?> getAllCountriesSorted()
+	@GetMapping(value = "/start/{letter}", produces = {"application/json"})
+	public ResponseEntity<?> getFilteredCountries(@PathVariable Character letter)
 	{
-		CountriesApplication.ourCountryList.countryList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
-		return new ResponseEntity<>(CountriesApplication.ourCountryList.countryList, HttpStatus.OK);
+		ArrayList<Country> rtnList = CountriesApplication.ourCountryList.findCountries(e -> e.getName().toUpperCase().startsWith(letter.toString().toUpperCase()));
+		rtnList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
+		return new ResponseEntity<>(rtnList, HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/size/{testLength}", produces = {"application/json"})
+	public ResponseEntity<?> getFilteredCountriesByNameLength(@PathVariable int testLength)
+	{
+		ArrayList<Country> rtnList = CountriesApplication.ourCountryList.findCountries(e -> e.getName().length() >= testLength);
+		rtnList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
+		return new ResponseEntity<>(rtnList, HttpStatus.OK);
+	}
+	@GetMapping(value = "/size/{testPopulation}", produces = {"application/json"})
+	public ResponseEntity<?> getFilteredCountriesByPopulation(@PathVariable int testPopulation)
+	{
+		ArrayList<Country> rtnList = CountriesApplication.ourCountryList.findCountries(e -> e.getPopulation() >= testPopulation);
+		rtnList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
+		return new ResponseEntity<>(rtnList, HttpStatus.OK);
 	}
 }
